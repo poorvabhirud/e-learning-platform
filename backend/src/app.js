f2bd2c7a227114e2b792645fedb8b72838f4d53a
Poorva Bhirud <poorva.bhirud_comp23@pccoer.in>
2026-01-20 20:20:04 +0530

    Configure API + env

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({  origin: [
+      "http://localhost:5173",
+      "https://e-learning-platform-cqvugnqlq-poorvabhiruds-projects.vercel.app"
+    ], credentials: true }));
 app.use(express.json());
 
 app.get("/api/health", (req, res) => {
